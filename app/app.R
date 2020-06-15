11ad61458afba1ed9315a119f0d83b064164d9e7
Katie C <katiecampbell@mednet.ucla.edu>
2020-06-15 12:47:15 -0700

    updated

--- app/app.R
+++ app/app.R
@@ -4,6 +4,8 @@ library(DT)
 library(data.table)
 library(plotly)
 library(tidyverse)
+library(leaflet)
+library(wesanderson)
 
 ##### ~~~~~ UI ~~~~~ #####
 ui <- dashboardPage(skin = "purple",
@@ -17,19 +19,32 @@ ui <- dashboardPage(skin = "purple",
                             selectInput("proteinInput", "Select a Protein", choices = c("Show All"), selected = "Show All")
                             )
            ),
-      
-      menuItem("Epitope", tabName = "epitope", icon = icon("flag")),
+      #
+      menuItem("Putative Epitopes", tabName = "epitope", icon = icon("flag")),
       div( id = 'sidebar_epitope',
            conditionalPanel("input.sidebar == 'epitope'"
                             # selectizeInput("epitopeInput", "Epitope of Interest", choices = NULL, selected = NULL, multiple = TRUE),
-                            
+
+                            )
+           ),
+      # 
+      menuItem("HLA", tabName = "hla",icon = icon("fingerprint")),
+      div( id = 'sidebar_hla',
+           conditionalPanel("input.sidebar == 'hla'",
+                            # textInput("hlaInput", "Search HLA", value = "HLA-A*01:01"),
+                            selectizeInput("hlaInput", "Search HLA", choices = c("A*68:44"), selected = "A*68:44"),
+                            selectInput("hlaProteinInput", "Select a Protein", choices = c("Show All"), selected = "Show All")
+                            )
+           ),
+      #
+      menuItem("Population", tabName = "populations", icon = icon("globe")),
+      div( id = 'sidebar_populations',
+           conditionalPanel("input.sidebar == 'populations'",
+                            selectizeInput("countryInput", "Country(s)", choices = c("United States"), selected = "United States", multiple = TRUE),
+                            numericInput("allelicFreq", "Minimum Population Allelic Frequency", 0.05, min = 0, max = 1)
                             )
            )
-      ),
-      
-      menuItem("HLA", tabName = "hla",icon = icon("fingerprint")),
-      
-      menuItem("Population", tabName = "populations", icon = icon("globe"))
+    )
   ),
   dashboardBody(
     tabItems(
@@ -37,7 +52,7 @@ ui <- dashboardPage(skin = "purple",
               box(title = "Viral View", width = 12,
                   plotlyOutput("mainPlotly")
                   ),
-              box(title = "Epitopes", width = 12,
+              box(title = "Predicted Epitopes", width = 12,
                   dataTableOutput("peptideDataTable")
                   )
               ),
@@ -49,11 +64,28 @@ ui <- dashboardPage(skin = "purple",
                     br(),
                     textInput("epitopeInput", "Find Epitope", value = ""),
                     width = 6, height = 300),
-                box(width = 6, height = 300)
+                box(width = 6, height = 300
+                    # textOutput("iedbTextOutput")
+                    )
               ),
-              box(title = "Filtered Epitope", width = 12,
+              box(title = "Predicted Epitopes", width = 12,
                   dataTableOutput("epitopeDataTable")
               )
+      ),
+      tabItem(tabName = "hla",
+              box(title = "HLA View", width = 12, height = 600,
+                  plotlyOutput("hlaPlotly")
+                  ),
+              box(title = "Predicted Epitopes", width = 12,
+                  checkboxInput("otherHlaFamilyMembers", "Show peptides from other family members"),
+                  dataTableOutput("hlaPeptideTable")
+                  # dataTableOutput("test")
+                  )
+              ),
+      tabItem(tabName = "populations",
+              box(leafletOutput("worldmap"), width = 12),
+              # box(dataTableOutput("alleleN"), width = 6),
+              box(dataTableOutput("alleleBreakdown"), width = 12)
       )
     )
   )
@@ -62,7 +94,11 @@ ui <- dashboardPage(skin = "purple",
 ##### ~~~~~ SERVER ~~~~~ #####
 # Define server logic required to draw a histogram ----
 server <- function(input, output, session) {
-  load('~/neocovid-app/app/data/forApp.Rda')
+  # load('~/neocovid-app/app/data/forApp.Rda')
+  load(file = url("https://github.com/kcampbel/neocovid-app/raw/master/app/data/forApp.Rda"))
+  # download.file(url = "https://github.com/kcampbel/neocovid-app/blob/master/app/data/forApp.Rda", "myfile")
+  # load("myfile")
+  
   
   ##### Viral View #####
   # Get protein name options
@@ -101,8 +137,9 @@ server <- function(input, output, session) {
       geom_rect(data = plotEpisData, aes(xmin = peptide.polar_start, xmax = peptide.polar_end, ymin = y_new, ymax = y_new + 0.5)) +
       #
       scale_x_continuous(limits = c(min(plotData$polar_start), max(plotData$polar_end))) + 
-      scale_y_continuous(limits = c(-8,max(plotEpis$y_new)+1)) +
-      theme(axis.text.y = element_blank(), axis.ticks.y = element_blank())
+      scale_y_continuous(limits = c(-8,max(plotEpis$y_new)+1)) + theme_bw() +
+      theme(axis.text.y = element_blank(), axis.ticks.y = element_blank()) +
+      facet_grid(Class ~ ., scales = 'free_y')
     #
     ggplotly(basePlot, source = "virusPlot") %>% layout(xaxis = list(rangeslider = list(type = "date")))
   })
@@ -142,7 +179,200 @@ server <- function(input, output, session) {
     }
   })
   
+  # subsetIedb <- reactive({
+  #   if(input$epitopeInput == "") {
+  #     return("No epitopes found in IEDB.")
+  #   } else {
+  #     sub_string <- grep(input$epitopeInput, uniqueIedbAnt, value = TRUE)
+  #     get_dist <- lapply(uniqueIedbAnt, function(y){ adist(input$epitopeInput, y) }) %>% unlist
+  #     closest_peptides <- uniqueIedbAnt[which(get_dist<3)]
+  #     message <- paste0(
+  #       ifelse(length(sub_string)>0, paste0("Epitope ", input$epitopeInput, " found in IEDB.\n"), ""),
+  #       "There were ", length(union(sub_string, get_dist), " epitopes that contain \'", input$epitopeInput,"\'.\n"),
+  #       stringr::str_wrap(paste0(union(sub_string, get_dist), collapse = ", "), 50)
+  #     )
+  #     return(message)
+  #   }
+  # })
+  
+  # output$iedbTextOutput <- renderText(subsetIedb())
+  
   output$epitopeDataTable <- renderDataTable(subsetEpitopes(), options = list(scrollX = TRUE, scrollY = TRUE))
+  
+  ##### HLA View #####
+  # Get HLA options
+  observe({
+    updateSelectizeInput(
+      session,
+      "hlaInput",
+      choices = unique(summPredictions$`HLA Allele`),
+      selected = "A*68:44"
+    )
+  })
+  
+  # Get protein name options
+  observe({
+    updateSelectInput(
+      session,
+      "hlaProteinInput",
+      choices = c("Show All", unique(as.character(proteinSequences3$Protein))),
+      selected = "Show All"
+    )
+  })
+
+  # Get HLA protein family
+  hlaFamily <- reactive({
+    getHlaFamily <- gsub("([A-C]\\*\\d+):\\d+", "\\1", input$hlaInput)
+    return(getHlaFamily)
+  })
+
+  # Filter to HLA family members
+  filtHlaFamily <- reactive({
+    filtHlaFamily <- summPredictions2 %>% ungroup %>%
+      mutate(HlaFamily = gsub("([A-C]\\*\\d+):\\d+", "\\1", `HLA Allele`)) %>%
+      filter(HlaFamily == hlaFamily() & `HLA Allele` != input$hlaInput)
+    return(filtHlaFamily)
+  })
+
+  # Subset to selected protein
+  subsetToProtein <- reactive({
+    if(input$hlaProteinInput == "Show All"){
+      subsetToProtein <- proteinSequences3
+    } else {
+      subsetToProtein <- filter(proteinSequences3, Protein == input$hlaProteinInput)
+    }
+    return(subsetToProtein)
+  })
+
+  output$hlaPlotly <- renderPlotly({
+    plotData <- subsetToProtein()
+
+    summHlaFamily <- filtHlaFamily() %>% group_by(Peptide, peptide.polar_start) %>% summarise(n = n())
+    filterPreds <- filter(summPredictions2, `HLA Allele` == input$hlaInput)
+
+    basePlot <- ggplot(data = NULL) +
+      geom_hline(data = NULL, yintercept = -25, colour = 'grey80', size = 2) +
+      # Peptides from the same HLA family members
+      geom_segment(data = summHlaFamily, aes(x = peptide.polar_start, xend = peptide.polar_start,
+                                             y = 510, yend = 590, colour = n)) +
+      scale_colour_gradientn(colours = wes_palette('Zissou1', 50, type = 'continuous')) +
+      # Chains
+      geom_rect(data = plotData, aes(fill = Gene, xmin = chain_polar_start, xmax = chain_polar_end,
+                                              ymin = -50, ymax = 0),
+                fill = 'grey80', alpha = 0.3) +
+      # Proteins
+      geom_rect(data = plotData, aes(fill = Gene, xmin = polar_start, xmax = polar_end,
+                                              ymin = -40, ymax = -10)) +
+      scale_fill_manual(values = sample(colors, 12)) +
+      # Peptides from specific HLA type
+      geom_col(data = filterPreds, aes(fill = Gene, x = peptide.polar_start,
+                                       y = 500-`Median Affinity (nM)`), width = 5) +
+      #
+      scale_x_continuous(limits = c(min(plotData$polar_start), max(plotData$polar_end))) +
+      scale_y_continuous(limits = c(-50,600), breaks = c(0, 100, 200, 300, 400, 500, 550),
+                         labels = c(500,400,300,200,100,0, paste0('Other ',hlaFamily()," Alleles"))) +
+      labs(y = "Predicted Binding Affinity (nM)", x = "Position in SARS-CoV-2",
+           colour = "N Other Alleles") +
+      theme_bw() + 
+      theme()
+
+    ggplotly(basePlot, source = "hlaPeptidePlot", height = 550) %>% 
+      layout(xaxis = list(rangeslider = list(type = "date")))
+  })
+  
+  # Subset predictions to those in view (whether it's full, a single protein, or zoomed in)
+  filterHlaPredictions <- reactive({
+    zoom <- event_data("plotly_relayout", "hlaPeptidePlot")
+    # if plot just rendered, event_data is NULL
+    # if user double clicks for autozoom, then zoom$xaxis.autorange is TRUE
+    # if user resizes page, then zoom$width is pixels of plot width
+    # filterPreds <- filter(summPredictions2, `HLA Allele` == pickHLA)
+    # filterPredsPlusHlaFamily <- filter(summPredictions2, `HLA Allele` == pickHLA | `HLA Allele` %in% filtHlaFamily$`HLA Allele`)
+    # 
+    if(input$hlaProteinInput == "Show All" & (is.null(zoom) || names(zoom[1]) %in% c("xaxis.autorange", "width"))) {
+      if(input$otherHlaFamilyMembers == TRUE) {
+        return(filter(summPredictions, `HLA Allele` == input$hlaInput | `HLA Allele` %in% filtHlaFamily()$`HLA Allele`))
+      } else {
+        return(filter(summPredictions, `HLA Allele` == input$hlaInput))
+      }
+    } else if(input$hlaProteinInput != "Show All") {
+      if(input$otherHlaFamilyMembers == TRUE) {
+        return(filter(summPredictions, Protein == input$hlaProteinInput & (`HLA Allele` == input$hlaInput | `HLA Allele` %in% filtHlaFamily()$`HLA Allele`)))
+      } else {
+        return(filter(summPredictions, Protein == input$hlaProteinInput & `HLA Allele` == input$hlaInput))
+      }
+    } else {
+      xmin <- zoom$`xaxis.range[0]`
+      xmax <- zoom$`xaxis.range[1]`
+      get_peptides <- filter(plotEpis, peptide.polar_start > xmin & peptide.polar_end < xmax)$Peptide
+      if(input$otherHlaFamilyMembers == TRUE) {
+        return(filter(summPredictions, Peptide %in% get_peptides & (`HLA Allele` == input$hlaInput | `HLA Allele` %in% filtHlaFamily()$`HLA Allele`)))
+      } else {
+        return(filter(summPredictions, Peptide %in% get_peptides & `HLA Allele` == input$hlaInput))
+      }
+    }
+  })
+  # Output predictions table
+  output$hlaPeptideTable <- renderDataTable(filterHlaPredictions(), options = list(scrollX = TRUE, scrollY = TRUE))
+
+  ##### Population View #####
+  mypalette <- colorFactor( palette=colors, domain=world@data$PICK, na.color = "transparent" )
+  mytext <- world@data$NAME %>% lapply(htmltools::HTML)
+  #
+  observe({
+    updateSelectizeInput(
+      session,
+      "countryInput",
+      choices = unique(filter(world@data, !is.na(PICK))$NAME),
+      selected = "United States"
+    )
+  })
+  #
+  getPopulationBreakdown <- reactive({
+    countryPopFreq %>%
+      mutate(Gene = paste0("HLA-", Locus)) %>%
+      filter(Country %in% input$countryInput & `Allele Frequency`>=input$allelicFreq)
+    # fulldf %>% 
+    #   mutate(Country = World_Country, Gene = paste0("HLA-", Locus)) %>%
+    #   dplyr::select(Region, Country, Population, `Sample Size`,
+    #                 Gene, Allele, Superfamily, 
+    #                 `Allele Frequency`,
+    #                 `% of individuals that have the allele`) %>%
+    #   filter(Country %in% input$countryInput & `Allele Frequency`>=input$allelicFreq)
+  })
+  #
+  getAlleleBreakdown <- reactive({
+    uniqueAlleles <- unique(getPopulationBreakdown()$`HLA Allele`)
+    
+  })
+  
+  #
+  autoZoomMap <- reactive({
+    world@data %>% filter(NAME %in% input$countryInput)
+  })
+  #
+  output$worldmap <- renderLeaflet({
+    leaflet(world) %>%
+      addProviderTiles(providers$CartoDB.Positron,
+      options = providerTileOptions(noWrap = TRUE)) %>%
+      addTiles() %>% 
+      setView(lng = autoZoomMap()$LON[1], lat = autoZoomMap()$LAT[1], zoom = 3) %>%
+      addCircleMarkers(~LON, ~LAT,
+                       fillColor = ~mypalette(PICK), 
+                       fillOpacity = 0.7, 
+                       color = "white", 
+                       radius = 8, 
+                       stroke = FALSE,
+                       label = mytext,
+                       labelOptions = labelOptions( style = list("font-weight" = "normal",
+                                                                 padding = "3px 8px"),
+                                                    textsize = "13px",
+                                                    direction = "auto")
+                       )
+  })
+  
+  # output$alleleN <- renderDataTable( getAlleleBreakdown() )
+  output$alleleBreakdown <- renderDataTable( getPopulationBreakdown() )
   
 }
 
--- app/createFuns.R
+++ app/createFuns.R
@@ -3,14 +3,15 @@ library(data.table)
 library(tidyverse)
 library(stringr)
 library(stringdist)
-setwd('~/projects/covid/app/')
+library(wesanderson)
+setwd('~/neocovid-app/app')
 
 colors <- c('#7ed6df','#535c68','#badc58','#30336b',
             '#f6e58d','#6ab04c','#22a6b3','#f9ca24',
             '#4834d4','#95afc0','#e056fd','#ff7979',
             '#be2edd','#c7ecee','#f0932b','#eb4d4b',
             '#130f40','#ffbe76','#dff9fb','#686de0')
-## SARS COV 2 PROTEIN OUTLINE
+##### SARS COV 2 PROTEIN OUTLINE #####
 proteinSequences <- fread('data/UP000464024.tsv')
 proteinSequences2 <- proteinSequences %>% mutate(prot_start = 1) %>% arrange(GenomePos) %>%
   mutate(polar_end = cumsum(Length) + 100*GenomePos,
@@ -43,6 +44,7 @@ ggplot(data = NULL) +
   theme_void()
 
 predictions <- fread('~/projects/covid/results/tables/SupplementaryTable3_FilteredAntigenBindingPredictions.tsv')
+# predictions <- fread('~/projects/covid/classii/results/Table_FilteredClassIIAntigenBindingPredictions.tsv')
 epitopes <- unique(predictions$Epitope.Seq)
 epiPositions <- data.table(Peptide = epitopes) %>% group_by(Peptide) %>%
   mutate(findPep = ifelse(length(grep(Peptide, proteinSequences2$Sequence))>0, grep(Peptide, proteinSequences2$Sequence), NA)) %>% 
@@ -85,21 +87,21 @@ plotEpis <- prepEpis %>% arrange(peptide.polar_start) %>%
   summarise(y = max(n)) %>% ungroup %>% arrange(peptide.polar_start) %>%
   mutate(y_new = ifelse(y == lag(y, default = 0), y-1, y))
 
-ggplot(data = NULL) +
-  geom_hline(yintercept = -1, colour = 'grey80', size = 2) +
-  # Proteins
-  geom_rect(data = proteinSequences3, aes(xmin = polar_start, xmax = polar_end, ymin = -1.5, ymax = -0.5, fill = Gene)) +
-  scale_fill_manual(values = sample(colors, 12)) +
-  # Chains
-  geom_rect(data = filter(proteinSequences3, polar_start != chain_polar_start | polar_end != chain_polar_end),
-            aes(xmin = chain_polar_start, xmax = chain_polar_end, ymin = -1.75, ymax = -0.25), fill = 'grey80', alpha = 0.3) +
-  # Peptides
-  geom_rect(data = plotEpis, aes(xmin = peptide.polar_start, xmax = peptide.polar_end, ymin = y_new, ymax = y_new + 0.5, fill = Gene)) +
-  #
-  scale_x_continuous(limits = c(0, max(proteinSequences3$polar_end))) + 
-  scale_y_continuous(limits = c(-10,max(plotEpis$y_new)+2)) +
-  coord_polar() +
-  theme_void()
+# ggplot(data = NULL) +
+#   geom_hline(yintercept = -1, colour = 'grey80', size = 2) +
+#   # Proteins
+#   geom_rect(data = proteinSequences3, aes(xmin = polar_start, xmax = polar_end, ymin = -1.5, ymax = -0.5, fill = Gene)) +
+#   scale_fill_manual(values = sample(colors, 12)) +
+#   # Chains
+#   geom_rect(data = filter(proteinSequences3, polar_start != chain_polar_start | polar_end != chain_polar_end),
+#             aes(xmin = chain_polar_start, xmax = chain_polar_end, ymin = -1.75, ymax = -0.25), fill = 'grey80', alpha = 0.3) +
+#   # Peptides
+#   geom_rect(data = plotEpis, aes(xmin = peptide.polar_start, xmax = peptide.polar_end, ymin = y_new, ymax = y_new + 0.5, fill = Gene)) +
+#   #
+#   scale_x_continuous(limits = c(0, max(proteinSequences3$polar_end))) + 
+#   scale_y_continuous(limits = c(-10,max(plotEpis$y_new)+2)) +
+#   coord_polar() +
+#   theme_void()
 summPredictions <- predictions %>% 
   group_by(Epitope.Seq, HLA.Allele) %>%
   summarise(`Median Affinity (nM)` = median(Median.Score, na.rm = T),
@@ -110,10 +112,29 @@ summPredictions <- predictions %>%
             `Stability Rank` = median(Stability.Rank, na.rm = T))
 summPredictions <- dplyr::select(plotEpis, Peptide, Gene, Protein, Position) %>%
   full_join(summPredictions, by = c("Peptide" = "Epitope.Seq"))
+# write.table(summPredictions, file = "~/projects/covid/results_si/int/summariseClassIpredictions.tsv", sep = "\t", quote=F, row.names = F)
 
+# CLASS II 
 
-lapply(notFoundEpis$Peptide, function(x){
-  all_dist <- lapply(foundEpis$Peptide, function(y){
+predictionsII <- fread('~/projects/covid/classii/results/Table_FilteredClassIIAntigenBindingPredictions.tsv')
+epitopesII <- unique(predictionsII$`Epitope Seq`) # 2,547
+epiPositionsII <- data.table(Peptide = epitopesII) %>% group_by(Peptide) %>%
+  mutate(findPep = ifelse(length(grep(Peptide, proteinSequences2$Sequence))>0, 
+                          grep(Peptide, proteinSequences2$Sequence), NA)) %>% 
+  # filter(!is.na(findPep)) %>%
+  mutate(Gene = proteinSequences2$Gene[findPep],
+         Position = str_locate(proteinSequences2$Sequence[findPep], Peptide)[1],
+         Position_end = str_locate(proteinSequences2$Sequence[findPep], Peptide)[2])
+foundEpisII <- epiPositionsII %>% filter(!is.na(findPep)) %>% mutate(findPep = NULL) # 2,348
+notFoundEpisII <- epiPositionsII %>% filter(is.na(findPep)) # 199
+# #
+# m =3
+# getMore <- epiPositions %>% filter(is.na(findPep)) %>%
+#   mutate(findPep = ifelse(length(agrep(Peptide, proteinSequences2$Sequence, max = m))>0, 
+#                           agrep(Peptide, proteinSequences2$Sequence, max =m), NA)) %>%
+#   filter(!is.na(findPep))
+getMoreII <- lapply(notFoundEpisII$Peptide, function(x){
+  all_dist <- lapply(foundEpisII$Peptide, function(y){
     adist(x, y)
   }) %>% unlist
   if(length(which(all_dist<2))>0) {
@@ -125,11 +146,147 @@ lapply(notFoundEpis$Peptide, function(x){
     return(d)
   }
 })
+moreEpisII <- Reduce(full_join, getMoreII) %>% filter(!is.na(closestPeptide)) %>% 
+  full_join(foundEpisII) #2,440
+prepEpisII <- moreEpisII %>% left_join(dplyr::select(proteinSequences2, Gene, Protein, polar_start, polar_end)) %>% 
+  group_by(Peptide) %>% 
+  mutate(peptide.polar_start = polar_start + Position, peptide.polar_end = polar_start + Position_end)
 
+plotEpisII <- prepEpisII %>% arrange(peptide.polar_start) %>%
+  mutate(all_pos = list(seq(peptide.polar_start, peptide.polar_end, by = 1))) %>% 
+  unnest(cols = c(all_pos)) %>%
+  group_by(all_pos) %>% mutate(n = 1:n()) %>% 
+  group_by(Peptide, closestPeptide, Gene, Protein, Position, Position_end, polar_start, polar_end, peptide.polar_start, peptide.polar_end) %>% 
+  summarise(y = max(n)) %>% ungroup %>% arrange(peptide.polar_start) %>%
+  mutate(y_new = ifelse(y == lag(y, default = 0), y-1, y))
 
+summPredictionsII <- predictionsII %>% 
+  group_by(`Epitope Seq`, `HLA Allele`) %>%
+  summarise(`Median Affinity (nM)` = median(`Median Score`, na.rm = T),
+            `Best Score (nM)` = median(`Best Score`, na.rm = T),
+            `Best Score Method` = unique(`Best Score Method`, na.rm = T)[1]
+            # `Predicted Stability (NetMHCstabpan)` = median(`Predicted Stability`, na.rm = T),
+            # `Half Life` = median(`Half Life`, na.rm = T),
+            # `Stability Rank` = median(`Stability Rank`, na.rm = T)
+            )
+summPredictionsII <- dplyr::select(plotEpisII , Peptide, Gene, Protein, Position) %>%
+  full_join(summPredictionsII, by = c("Peptide" = "Epitope Seq"))
+# write.table(summPredictionsII, file = "~/projects/covid/results_si/int/summariseClassIIpredictions.tsv", sep = "\t", quote=F, row.names = F)
 
+##### HLA View #####
+summPredictions2 <- dplyr::select(plotEpis, Peptide, Gene, Protein, Position, peptide.polar_start, peptide.polar_end) %>%
+  full_join(summPredictions)
+summPredictions2II <- dplyr::select(plotEpisII, Peptide, Gene, Protein, Position, peptide.polar_start, peptide.polar_end) %>%
+  full_join(summPredictionsII)
+pickHLA = "HLA-A*02:02"
+getHlaFamily <- gsub("(HLA-[A-C]\\*\\d+):\\d+", "\\1", pickHLA)
+filtHlaFamily <- summPredictions2 %>% ungroup %>% mutate(HlaFamily = gsub("(HLA-[A-C]\\*\\d+):\\d+", "\\1", HLA.Allele)) %>%
+  filter(HlaFamily == getHlaFamily & HLA.Allele != pickHLA)
+summHlaFamily <- filtHlaFamily %>% group_by(Peptide, peptide.polar_start) %>% summarise(n = n())
+filterPreds <- filter(summPredictions2, HLA.Allele == pickHLA)
+filterPredsPlusHlaFamily <- filter(summPredictions2, HLA.Allele == pickHLA | HLA.Allele %in% filtHlaFamily$HLA.Allele)
 
-save(colors, proteinSequences3, plotEpis, summPredictions, file = 'data/forApp.Rda')
+ggplot(data = NULL) +
+  geom_hline(data = NULL, yintercept = -25, colour = 'grey80', size = 2) +
+  # Peptides from the same HLA family members
+  geom_segment(data = summHlaFamily, aes(x = peptide.polar_start, xend = peptide.polar_start,
+                                         y = 510, yend = Inf, colour = n)) +
+  scale_colour_gradientn(colours = wes_palette('Zissou1', 50, type = 'continuous')) +
+  # Chains
+  geom_rect(data = proteinSequences3, aes(fill = Gene, xmin = chain_polar_start, xmax = chain_polar_end,
+                                          ymin = -50, ymax = 0),
+            # ymin = -1.75, ymax = -0.25),
+            fill = 'grey80', alpha = 0.3) +
+  # Proteins
+  geom_rect(data = proteinSequences3, aes(fill = Gene, xmin = polar_start, xmax = polar_end,
+                                          ymin = -40, ymax = -10)) +
+                                          # ymin = -1.5, ymax = -0.5)) +
+  scale_fill_manual(values = sample(colors, 12)) +
+  # Peptides from specific HLA type
+  geom_col(data = filterPreds, aes(fill = Gene, x = peptide.polar_start, 
+                                    y = 500-`Median Affinity (nM)`), width = 5) +
+  #
+  scale_x_continuous(limits = c(min(proteinSequences3$polar_start), max(proteinSequences3$polar_end))) +
+  scale_y_continuous(limits = c(-50,550), breaks = c(0, 100, 200, 300, 400, 500, 550),
+                     labels = c(500,400,300,200,100,0, paste0('Other ',getHlaFamily," Alleles"))) +
+  labs(y = "Predicted Median Affinity (nM)", x = "Position in SARS-CoV-2",
+       colour = "N Other Alleles") +
+  theme()
+
+#####
+# fulldf <- fread('data/tb_for_katie/fulldf.tsv')
+freqcountry <- fread('data/tb_for_katie/freq_country_V2.tsv')
+perccountry <- fread('data/tb_for_katie/percent_country.tsv')
+fulldf <- fread('data/tb_for_katie/fulldf.txt')
+#
+# download.file("http://thematicmapping.org/downloads/TM_WORLD_BORDERS_SIMPL-0.3.zip" , destfile="data/world_shape_file.zip")
+library(rgdal)
+world <- readOGR(dsn = "data/",
+                 layer = "TM_WORLD_BORDERS_SIMPL-0.3",
+                 verbose = FALSE)
+# setdiff(fulldf2$World_Country, world$NAME)
+# grep('Samoa', world$NAME, value = TRUE)
+fulldf <- fulldf %>% mutate(NAME = World_Country)
+summ_pops <- fulldf %>% group_by(NAME) %>% 
+  summarise(PICK = sample(1:length(colors), 1, replace = TRUE))
+world@data <- world@data %>% left_join(summ_pops) %>%
+  mutate(PICK = factor(PICK))
+
+#####
+plotEpisI = plotEpis %>% mutate(Class = "I")
+summPredictionsI = summPredictions %>% mutate(Class = "I") %>% rename("HLA Allele" = "HLA.Allele")
+summPredictions2I = summPredictions2 %>% mutate(Class = "I") %>% rename("HLA Allele" = "HLA.Allele")
+
+plotEpis <- plotEpisII %>% mutate(Class = "II") %>% 
+  full_join(plotEpisI) %>%
+  mutate(Class = factor(Class, levels = c('I','II')))
+summPredictions <- summPredictionsII %>% mutate(Class = "II") %>% 
+  full_join(summPredictionsI) %>%
+  mutate(Class = factor(Class, levels = c('I','II')),
+         `HLA Allele` = gsub("HLA-", "", `HLA Allele`))
+summPredictions2 <- summPredictions2II %>% mutate(Class = "II") %>% full_join(summPredictions2I) %>%
+  mutate(Class = factor(Class, levels = c('I','II')),
+         `HLA Allele` = gsub("HLA-", "", `HLA Allele`))
+
+###
+iedb <- fread('data/iedb20200602.epitope_full_v3.csv', skip = 1)
+uniqueIedbAnt <- unique(iedb$Description)
+
+# getMoreII <- lapply(unique(summPredictions$Peptide), function(x){
+#   find <- lapply(uniqueIedbAnt, function(y){
+#     if ()
+#     grepl(x, y)
+#   }) %>% unlist
+#   find_filt <- Filter(Negate(is.null), find)
+#   all_dist <- lapply(uniqueIedbAnt, function(y){
+#     adist(x, y)
+#   }) %>% unlist
+#   if (length(find_filt)>0) {
+#     return()
+#   } else if(length(which(all_dist<2))>0) {
+#     d <- data.frame(Peptide = x, closestPeptide = foundEpis$Peptide[which(all_dist<2)])
+#     e <- left_join(d, foundEpis, by = c('closestPeptide' = 'Peptide'))
+#     return(e)
+#   } else {
+#     d <- data.frame(Peptide = x, closestPeptide = as.character(NA))
+#     return(d)
+#   }
+# })
+###
+tm <- fulldf %>% group_by(Country, World_Country) %>% summarise(n=n()) %>%
+  dplyr::select(Country, World_Country)
+countryPopFreq <- freqcountry %>% left_join(tm) %>% 
+  dplyr::select(World_Country, Region, Locus, Superfamily, Allele, Country_Frequency, country_n)
+colnames(countryPopFreq) <- c('Country','Region', 'Locus','HLA Superfamily', 'HLA Allele',
+                              "Allele Frequency", "N Individuals (Sampled)")
+#
+
+save(world, fulldf, countryPopFreq,
+     colors,
+     proteinSequences3, 
+     plotEpis, summPredictions, summPredictions2,
+     # uniqueIedbAnt,
+     file = 'data/forApp.Rda')
 
 
 
